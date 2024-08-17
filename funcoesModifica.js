06d53d92aac818d3b6e2166344a8f73a1b6c6302
Leticiabeatriz15 <leticiabeatriz1508@gmail.com>
2024-08-17 17:24:24 -0300

    Adicionando a data na função adiciona

--- funcoesModifica.js
+++ funcoesModifica.js
@@ -9,30 +9,42 @@ export function adiciona(){
     const opc = prompt('Deseja colocar a descrição de sua tarefa? Digite "s" para Sim ou "n" para Não...\n').trim().toLowerCase()
     opc == 's' ? desc = prompt('Escreva a descrição de sua tarefa: ').trim().toLowerCase(): desc = 'Não há descrição para essa atividade';
 
-    let venc = Number(prompt('Digite o número de dias  que falta para acabar o prazo de sua atividade:\n'))
-    venc != 0? venc = venc : venc = null;
+    let data = prompt('Digite a data de entrega:(coloque na forma DD/MM/AAAA)\n-> ').trim()
+    data != ''? dt(data) : data = null;
 
+
+    function dt(dados){
+        let datalocal
+        let quebraData = dados.split('/')
+        
+        datalocal = new Date(quebraData[2], quebraData[1] - 1, quebraData[0]);
+
+        return datalocal.toLocaleString("pt-BR", {timeZone : "America/Sao_Paulo"});
+    }
+
+    
     let nivel = Number(prompt('Qual a prioridade de sua tarefa?(Digite o número correspodente)\n1 - Baixa\n2 - Média\n3 - Alta\n-> '))
     nivel == 3? nivel = 'alta':(nivel == 2? nivel = 'media' : (nivel == 1? nivel = 'baixa' : nivel = null)); 
 
-    while(atividade.length === 0 || venc === null|| nivel === null ){
-        console.log('Funções obrigatórias em branco!')
-        if(atividade.length === 0){
-            atividade = prompt('Digite sua tarefa:\n° ').trim().toLowerCase()
-            return atividade
+    while(atividade.length == 0 || data == null|| nivel == null ){
 
-        }else if(venc === null){
-            venc = Number(prompt('Digite o número de dias  que falta para acabar o prazo de sua atividade:\n'))
-            venc != 0? venc = venc : venc = null;
+        console.log('Funções obrigatórias em branco(ou digitadas erradas)!')
         
-        }else if(nivel === null){
+        if(atividade.length == 0){
+            atividade = prompt('Digite sua tarefa:\n° ').trim().toLowerCase()
+
+        }else if(data == null){
+            data = prompt('Digite a data de entrega:(coloque na forma DD/MM/AAAA)\n-> ').trim()
+            data != ''? dt(data): data = null;
+        
+        }else if(nivel == null){
             nivel = Number(prompt('Qual a prioridade de sua tarefa?(Digite o número correspodente)\n1 - Baixa\n2 - Média\n3 - Alta\n-> '))
-            nivel == 3? nivel = 'alta':(nivel == 2? nivel = 'media' : nivel ='baixa');
+            nivel == 3? nivel = 'alta':(nivel == 2? nivel = 'media' : (nivel == 1? nivel = 'baixa' : nivel = null));
         }
     }
 
-    tarefas.push({titulo: atividade, descricao: desc, vencimento: venc, prioridade: nivel, status: false})
-
+    (atividade.length != 0 && data != null && nivel != null)? tarefas.push({titulo: atividade, descricao: desc, vencimento: data, prioridade: nivel, status: false}): console.log('ERROR');
+    
     console.table(tarefas)
 };
 
